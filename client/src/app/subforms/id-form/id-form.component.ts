3d13a6135550d9b8f97391dc22fffa96c176603e
Unknown <dkv8315@gmail.com>
2018-06-26 21:49:08 -0500

    Created the array to hold the different pieces of info for the client to fill out. Created text areas for the client to fill in their information

--- client/src/app/subforms/id-form/id-form.component.ts
+++ client/src/app/subforms/id-form/id-form.component.ts
@@ -6,6 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./id-form.component.css']
 })
 export class IDFormComponent implements OnInit {
+  IDForm = {
+	requestType: {
+      IDCard:'CoSA ID Card',
+      facultyAccess: 'CoSA Faculty Access',
+      cjisAccess: "CJIS Faculty Access",
+      facultyAccChange: 'Faculty Access Change',
+      issueID: 'Re-Issue ID Card'
+    },
+    personnelInfo: {
+        First:'First Name:',
+        middleNm: 'Middle Initial:',
+        Last: 'Last Name:',
+        Suffix: 'Suffix:',
+        jobStatus:'Job Status:',
+        workPhone:'Work Telephone Number:',
+        jobTitle: 'Job Title:',
+        Department:' Department:',
+        hrSAP:'CoSA/HR SAP#:',
+        Division: 'Division',
+        superNm: 'Supervisor/Manager Name:',
+        contactPhone: 'Contact Phone Number:',
+      },
+      facultyInfo: {
+        facultyNm:'Faculty Name:',
+        facultyAddr: 'Facility Address: ',
+        facultyEntry: 'Facility Entry: ',
+        suiteEntry: ' Suite Entry: ',
+        roomEntry: ' Room Entry: ',
+        other: 'Other: ',
+      },
+     departmentApprov: {
+     superSign: 'Supervisor/Manager Signature: ',
+     date: ' Date:'
+   }
+
+  }
 
   constructor() { }
 
