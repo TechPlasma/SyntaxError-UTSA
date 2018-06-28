3e8592a4f8445cc8cea6950d548c30faebf79da1
Saul Torres <sault1000@gmail.com>
2018-06-28 08:01:01 -0500

    Syncing Computers

--- client/src/app/subforms/sap-form/sap-form.component.ts
+++ client/src/app/subforms/sap-form/sap-form.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SAPFormComponent implements OnInit {
 
+	testObject={
+		section1:{
+			value:"",
+			items: [
+				{
+					name:"Add New User",
+					value:"Add New User"
+				},
+				{
+					name:"Modify Existing User (Add Roles)",
+					value:"Modify Existing User (Add Roles)"
+				},
+
+				{
+					name:"Delimt Roles",
+					value:"Delimt Roles"
+				},
+				{
+					name:"Deactivate Uer",
+					value:"Deactivate Uer"
+				},
+				{
+					name:"New Field",
+					value:"Deactivate"
+				}
+			]
+		}
+	}
+
+	alert(value){
+		console.log(value);
+	}
+
   constructor() { }
 
   ngOnInit() {
