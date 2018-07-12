7989f1e1ab0f8fdaab3930aa15215f7211b6e21f
Jesse <parker7th@gmail.com>
2018-07-12 15:37:01 -0500

    I'm starting the missing form
    Its the hardware software form.

--- client/src/app/subforms/hard-soft-form/hard-soft-form.component.ts
+++ client/src/app/subforms/hard-soft-form/hard-soft-form.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { hardSoftForm  } from './hard-softModel'
 
-@Component({
+@Component({	
   selector: 'app-hard-soft-form',
   templateUrl: './hard-soft-form.component.html',
   styleUrls: ['./hard-soft-form.component.css']
 })
 export class HardSoftFormComponent implements OnInit {
+	
+	hsForm: hardSoftForm = {
+		//request info. 
+		department: ``,
+		date: null,
+		depRequestor: ``,
+		phoneExt: ``,
+
+		//checkboxs:
+		provHardware: false,
+		provSoftware: false,
+
+		//fields for those checkboxs:
+		hardCostCenterIO: ``,
+		hardGlAcc: ``,
+
+		softCostCenterIO: ``,
+		softGlAcc: ``,
+
+		userName: `testing object!`,
+		SAPLogonId: ``,
+		formPosition: ``,
+
+		BldgLoc: ``,
+		floor: ``,
+		office: ``,
+
+		requestType: ``,
+		currentDeviceId: ``, 
+		//box = Checkbox and Input = dropdown menu or basic input. 
+		desktopBox: false,
+		desktopInput: ``,
+
+		laptopBox: false,
+		laptopInput: ``,
+		//Laptop box onlocks new checkboxes.
+		dockingStation: false,
+		keyMouse: false,
+		addMonitor: false,
+
+		monitorBox: false,
+		monitorInput: ``,
+		//monitor checkbox unlocks a 'Mode' dropdown menu. 
+		monitorMode: ``,
+
+		printerBox: false,
+		printerInput: ``,
+		ipDeskPhoneBox: false,
+		ipDeskPhoneInput: ``,
+	}//end hardSoftForm
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
+
+
 
 }
---
+++ client/src/app/subforms/hard-soft-form/hard-softModel.ts
@@ -0,0 +1,52 @@
+
+export class hardSoftForm{
+	department: string;
+	date: any;
+	depRequestor: string;
+	phoneExt: string;
+
+	//checkboxs:
+	provHardware: boolean;
+	provSoftware: boolean
+
+	//fields for those checkboxs:
+	hardCostCenterIO: string;
+	hardGlAcc: string;
+
+	softCostCenterIO: string;
+	softGlAcc: string;
+	
+	userName: string;
+	SAPLogonId: string;
+	formPosition: string;
+
+	BldgLoc: string;
+	floor: string;
+	office: string;
+
+	requestType: string;
+	currentDeviceId: string; 
+
+	//checkboxes in this section of the form.
+	desktopBox: boolean;
+	desktopInput: string;
+
+	laptopBox: boolean;
+	laptopInput: string;
+	//Laptop box onlocks new checkboxes.
+	dockingStation: boolean;
+	keyMouse: boolean;
+	addMonitor: boolean;
+
+	monitorBox: boolean;
+	monitorInput: string;
+	//monitor checkbox unlocks a 'Mode' dropdown menu. 
+	monitorMode: string;
+
+	printerBox: boolean;
+	printerInput: string;
+
+	ipDeskPhoneBox: boolean;
+	ipDeskPhoneInput: string;
+
+}
