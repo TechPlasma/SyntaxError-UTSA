fb2c74ec407f90b089449f6872b2acb2a59ed904
Saul Torres <sault1000@gmail.com>
2018-07-12 11:44:17 -0500

    Implemented Login and New View
    Created a new starting point and Login framework for displaying department and User information and forms. Linked everything together.

--- client/src/app/user-login/user-login.component.ts
+++ client/src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+	username:string;
+	password:string;
 
-  ngOnInit() {
-  }
+	login(){
+		this.router.navigate(['/view/']);
+	}
+
+	constructor(private router:Router) {
+
+	}
+
+	ngOnInit() {
+	}
 
 }
