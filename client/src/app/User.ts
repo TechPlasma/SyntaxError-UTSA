bb2ee973c034aac3c760e2ecbbe60cd6ade7c62e
Saul Torres <sault1000@gmail.com>
2018-07-17 12:27:37 -0500

    Update of User.ts
    Updated User.ts with what was learned making the Master Form Object

--- client/src/app/User.ts
+++ client/src/app/User.ts
@@ -1,13 +1,37 @@
 
 
 export class User{
-	UID:number;
-	DEPT:string;
-	POSIT:string;
+	// Constructor Calls
+	// 		new User()
+	// 		new User(FirstName)
+	// 		new User(FirstName,LastName)
+	// 		new User(FirstName,LastName,DEPT)
+	// 		new User(FirstName,LastName,DEPT,POSIT)
+
+	constructor(private FIRSTNAME?:string,private LASTNAME?:string,private DEPT?:string,private POSIT?:string){
+		if(FIRSTNAME == undefined)
+			this.FIRSTNAME = '';
+		if(LASTNAME == undefined)
+			this.LASTNAME = '';
+		if(DEPT == undefined)
+			this.DEPT = '';
+		if(POSIT == undefined)
+			this.POSIT = '';
+
+		this.UID = '';
+		this.PWD = '';
+		this.MI = '';
+		this.EMAIL = '';
+	}
+
+	//DEPT:string;
+	//POSIT:string;
+	UID:string;
 	PWD:string;
 
-	FIRSTNAME:string;
-	LASTNAME:string;
+	// FIRSTNAME:string;
+	// LASTNAME:string;
+	MI:string;
 	EMAIL:string;
 
 }
