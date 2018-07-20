6fa18a2fa7107bea929585fe2956d70d2b9aec5a
Saul Torres <sault1000@gmail.com>
2018-07-20 16:21:05 -0500

    MasterForm Object
    Finalized the Master Form Object. Linked everything.

--- client/src/app/form/form.component.ts
+++ client/src/app/form/form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { MasterForm, Approver } from './MasterFormTesting';
+import { MasterForm, Approver } from '../MasterForm';
 
 @Component({
   selector: 'app-form',
